ef637ccfb43681284eb153823e62dc0e06ea1249
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-04-23 20:56:22 +0800

    Create a003.cpp

---
+++ a003. 兩光法師占卜術/a003.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+int M,D,s;
+while(cin>>M>>D)
+{ 
+	s=(M*2+D)%3;
+	if (s==0)
+	cout<<"普通"<<endl;
+	if (s==1)
+	cout<<"吉"<<endl;
+	if (s==2)
+	cout<<"大吉"<<endl;
+} 
+return 0;
+
+}
