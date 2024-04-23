d6b57753018a77903c5a989e7abf39f284b20f1f
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-04-23 20:58:17 +0800

    Create a006.cpp

---
+++ a006. 一元二次方程式/a006.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+int a,b,c,d;
+int x1,x2;
+while(cin>>a>>b>>c)
+{
+if(b*b-4*a*c<0)
+	cout<<"No real root"<<endl;
+else if(b*b-4*a*c==0)
+{
+	cout<<"Two same roots x="<<-b/(a*2);
+}
+else if((b*b-4*a*c)>0)
+{
+	x1=(-b+sqrt(b*b-4*a*c))/(2*a);
+	x2=(-b-sqrt(b*b-4*a*c))/(2*a);
+}
+if (x1>x2)
+	cout<<"Two different roots x1="<<x1<<" , x2="<<x2;
+if (x1<x2)
+	cout<<"Two different roots x2="<<x2<<"x1="<<x1;
+}
+//sqrt(欲開根號的值，或變數)
+return 0;
+} 
+
