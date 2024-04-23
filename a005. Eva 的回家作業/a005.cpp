8bb56bdb4d2d1bf6384292ebc3c84cadea52a9f9
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-04-23 20:57:52 +0800

    Create a005.cpp

---
+++ a005. Eva 的回家作業/a005.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+int t;
+int i,a,b,c,d=0,e,f;
+while(cin>>t)
+{ 
+while(t-->=1)
+{
+	cin>>a>>b>>c>>d;
+	if(b-a==c-b)
+		e=2*d-c; 
+	else if (c/b==b/a)
+		e=d*(c/b);
+	cout<<a<<" "<<b<<" "<<c<<" "<<d<<" "<<e;
+	cout<<endl;
+}
+}
+return 0;
+} 
+
