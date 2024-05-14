37249eb11371692d6cd371ba3e43c018b2403a4c
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-05-14 18:00:36 +0800

    Update d872.cpp

--- d872. 過橋問題/d872.cpp
+++ d872. 過橋問題/d872.cpp
@@ -1,1 +1,43 @@
-  
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    while(cin>>t)
+    {
+    	if (t == 0){
+            cout << 0 << "\n";
+            continue;
+        }
+    	vector<int> n(t);
+    	for(int i=0;i<t;i++)
+    	{
+    		cin>>n[i];
+		}
+		sort(n.begin(),n.end());
+		int ans=0;
+		while(n.size()>3)
+		{
+			int x=n[0]+2*n[1]+n[n.size()-1];
+			int y=2*n[0]+n[n.size()-1]+n[n.size()-2];
+			ans+=min(x,y);
+			n.resize(n.size()-2); 
+		}
+		if(n.size()==3)
+		{
+			for(int i:n)
+			{
+				ans+=i;
+			}
+		}
+		else if(n.size()==2)
+		{
+			ans+=n[1];
+		}
+		else
+		{
+			ans+=n[0];
+		}
+		cout<<ans<<endl;
+	}
+    
+}
