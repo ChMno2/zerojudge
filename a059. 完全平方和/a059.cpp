c5272bda27ba0cb228f326eab3a2fadaad6f1d19
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-05-29 16:45:18 +0800

    Create a059.cpp

---
+++ a059. 完全平方和/a059.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    for (int j = 0; j < n; j++)
+    {
+        cout << "Case " << j+1 << ": ";
+        int a, b;
+        cin >> a >> b;
+        int ct = 0;
+        for (int i = a; i <= b; i++)
+        {
+            if (pow(i, 0.5) == int(pow(i, 0.5)))
+                ct += i;
+        }
+        cout << ct << '\n';
+    }
+}
