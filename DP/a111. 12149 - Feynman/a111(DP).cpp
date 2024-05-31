bb579d19281991d79aff4d9460f9d91500bc0345
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-05-31 22:01:35 +0800

    Create a111(DP).cpp

---
+++ DP/a111. 12149 - Feynman/a111(DP).cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<long long> dp(105, -1);
+long long DP(int n)
+{
+    if (dp[n] != -1)
+        return dp[n];
+    else
+    {
+        dp[n] = n * n + DP(n - 1);
+        return dp[n];
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int n;
+    dp[1] = 1;
+    while (cin >> n && n)
+    {
+        cout << DP(n) << '\n';
+    }
+}
