494a1731cf138388cbb77edfcf359994de1b7be9
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-06-05 00:16:58 +0800

    Update b565.cpp

--- b565. 5.採蘑菇攻略問題/b565.cpp
+++ b565. 5.採蘑菇攻略問題/b565.cpp
@@ -9,3 +9,15 @@ int main()
 	{
 		int mx=0,total=0;
 		for(int i=0;i<n;i++)
+		{
+			int s;
+			cin>>s;
+			total+=s;
+			mx=max(mx,total);
+			if(total<0)
+				total=0;	
+		}	
+		cout<<mx<<endl;
+	}
+		
+}
