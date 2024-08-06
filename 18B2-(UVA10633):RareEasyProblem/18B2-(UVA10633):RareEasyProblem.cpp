dd7daaf8f3116d2008d3ef1fc50391018cb8241d
MnO2 <57294562+ChMno2@users.noreply.github.com>
2024-08-07 01:09:23 +0800

    Create 18B2-(UVA10633):RareEasyProblem.cpp

---
+++ 18B2-(UVA10633):RareEasyProblem/18B2-(UVA10633):RareEasyProblem.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+	ll s;
+	while(cin>>s&&s!=0)
+	{
+		ll count=0;
+		if(s%9==0)
+			cout<<s*10/9-1<<" "<<s*10/9<<endl;
+		else
+			cout<<s*10/9<<endl;
+	}
+}
